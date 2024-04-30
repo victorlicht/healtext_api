eea622c4307c0a2cd406a04a009c1f5f02cab6aa
victorlicht <mihoubiabderrahmane09@gmail.com>
2024-04-30 16:58:13 +0100

    fix: Improved the upload

--- app/routers/uploads.py
+++ app/routers/uploads.py
@@ -1,8 +1,12 @@
-from fastapi import Depends, File, UploadFile, Form
+from fastapi import Depends, File, UploadFile, Form, status
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 import configs.db as db
 import utils.upload_ehr as upload
 from fastapi import APIRouter
+import logging
+logger = logging.getLogger(__name__)
+
 
 router = APIRouter(
     prefix="/documents",
@@ -24,5 +28,24 @@ async def upload_ehr(file: UploadFile = File(...),
     Returns:
         dict: Dictionary containing information about the uploaded document or an error message.
     """
-    upload_result = upload.upload_document(session, file, uid)
-    return upload_result
+    
+
+    try:
+        uploaded = upload.upload_document(session, file, uid)
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error: {str(e)}")
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={"error": "can't upload file, try later."},
+            media_type="application/json"
+        )
+    
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "message": "Document uploaded successfully",
+            "id": uploaded
+        },
+        media_type="application/json"
+    )
--- app/utils/upload_ehr.py
+++ app/utils/upload_ehr.py
@@ -3,11 +3,9 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 from werkzeug.utils import secure_filename
 from models.models import EhrUpload
-import logging
 import uuid
 
 def upload_document(db_session: sessionmaker, file_obj, uid):
-    logger = logging.getLogger(__name__)
 
     # Check if file is uploaded
     if not file_obj:
@@ -22,26 +20,17 @@ def upload_document(db_session: sessionmaker, file_obj, uid):
     data_dir = os.path.join("data", "ehr")
 
     # Generate UUID for both filename and id
-    upload_id = uuid.uuid4()  # Generate UUID
+    upload_id = str(uuid.uuid4())  # Generate UUID
+    print(type(upload_id))
+    
 
-    try:
-        # Create a new upload record with the same UUID as id
-        upload_record = EhrUpload(id=upload_id, uid=uid, doc_format=".txt", doc_url=os.path.join(data_dir, f"{upload_id}.txt"))
-        db_session.add(upload_record)
+    upload_record = EhrUpload(id=upload_id, uid=uid, doc_format=".txt", doc_url=os.path.join(data_dir, f"{upload_id}.txt"))
+    db_session.add(upload_record)
 
-        # Save file to data directory
-        filepath = os.path.join(data_dir, f"{upload_id}.txt")
-        with open(filepath, "wb") as f:
-            f.write(file_obj.file.read())
-        
-        db_session.commit()
-        logger.info("Upload record saved successfully.")
-        return {"message": f"SUCCESS: {filename}"}
-    except IntegrityError as e:
-        db_session.rollback()  # Rollback the transaction to maintain integrity
-        logger.error(f"IntegrityError: {str(e)}")
-        return {"error": "Failed to save upload record in database due to duplicate data or constraint violation."}
-    except Exception as e:
-        db_session.rollback()  # Rollback the transaction in case of other errors
-        logger.error(f"Error saving file: {str(e)}")
-        return {"error": f"Error saving file: {str(e)}"}
+    # Save file to data directory
+    filepath = os.path.join(data_dir, f"{upload_id}.txt")
+    with open(filepath, "wb") as f:
+        f.write(file_obj.file.read())
+    
+    db_session.commit()
+    return upload_record.id
